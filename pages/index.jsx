792d4bd6ffdcc09fae72236c14bb0c12348a14d1
Pavel <juicycode.webdev@gmail.com>
2022-11-22 20:20:44 +0100

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Bund-fiB</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    
+      {/* Navigationsleiste */}
+
+      {/* Titelseite */}
+
+      {/* Leistungsübersicht */}
+
+      {/* Lernförderung */}
+
+      {/* Privatunterricht */}
+
+      {/* Bildungsprojekte */}
+
+      {/* Über Uns */}
+
+      {/* Unser Teams */}
+
+      {/* Jobs */}
+
+      {/* Aktuelles */}
+
+      {/* Termin Formular */}
+
+      {/* Kontakt Formular */}
+
+      {/* Downloads */}
+
+      {/* Footer */}
+
+    </div>
+  )
+}

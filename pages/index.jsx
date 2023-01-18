edd0a38e9f9ea3577859b7593f690a7c634ef30d
Pavel <mail@pavelbuilds.com>
2023-01-18 14:49:14 +0100

    umami added (hosted on umami cloud)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
+
 import { useState, useEffect } from 'react';
 // Import Components
 import Container from '../components/Container';

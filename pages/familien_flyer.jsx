429b99069b732df103530cdaf5f978a7b9e34764
Pavel <mail@pavelbuilds.com>
2024-10-09 13:32:10 +0200

    flyer update

--- pages/familien_flyer.jsx
+++ pages/familien_flyer.jsx
@@ -1,13 +1,8 @@
 function Flyer() {
   return (
-    <div>
-      <iframe
-        src='/familien_flyer.pdf'
-        width='100%'
-        height='100%'
-        className='h-screen w-screen'
-        style={{ border: 'none' }}
-      />
+    <div className='h-screen w-screen'>
+      {/* <iframe src='/familien_flyer.pdf' width='100%' height='100%' style={{ border: 'none' }} /> */}
+      <iframe src='/familien_flyer.pdf' height='100%' width='100%'></iframe>
     </div>
   );
 }
